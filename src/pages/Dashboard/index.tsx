fae970514d9e13593af379c92978ccd6151a3e3e
ismael <ismael.vargas@fundacred.org.br>
2022-07-12 02:29:54 -0300

    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form } from "./styles";
+import { Title, Form, Repositories } from "./styles";
 
 const Dashboard: React.FC = () => {
     return (
@@ -14,6 +15,33 @@ const Dashboard: React.FC = () => {
                 <input placeholder="Digite o nome do repositório" />
                 <button type="submit">Pesquisar</button>
             </Form>
+
+            <Repositories>
+                <a href="teste">
+                    <img
+                        src="https://avatars.githubusercontent.com/u/50303514?v=4"
+                        alt="Ismael Vargas"
+                    />
+                    <div>
+                        <strong>PayWall - Frontend</strong>
+                        <p>Automatização de pagamentos</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+                <a href="teste">
+                    <img
+                        src="https://avatars.githubusercontent.com/u/50303514?v=4"
+                        alt="Ismael Vargas"
+                    />
+                    <div>
+                        <strong>PayWall - Frontend</strong>
+                        <p>Automatização de pagamentos</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
         </>
     );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -23,6 +23,7 @@ export const Form = styled.form`
     border: 0;
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
+    background-color: #f8f6f6;
     
     &::placeholder{
       color: #a8a8b3;
@@ -41,5 +42,56 @@ export const Form = styled.form`
     &:hover {
       background: ${shade(0.2, '#04d361')};
     }
+  }
+`;
+
+export const Repositories = styled.div `
+  margin-top: 80px;
+  border-radius: 5px;
+  
+  a {
+    background-color: #f8f6f6;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+  
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+    
+    & + a {
+      margin-top: 16px;
+    }
+    
+    &:hover {
+      transform: translateX(10px);
+    }
+  
+    img {
+      width: 67px;
+      height: 67px;
+      border-radius: 50%;
+    }
+      
+    div {
+      margin-left: 16px;
+        
+      strong {
+        font-size: 20px;
+        color: #3D3D4D;
+      }
+        
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+      
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }      
   }
 `;
